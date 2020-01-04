3f6e69a06ddfbd6bffdd5725873a3a39920ff6dd
sergeyBalotnikov <sergey.balotnikov@gmail.com>
2020-01-04 17:24:20 +0300

    Homework 1 fix

--- Homework1/app/src/main/java/ru/mail/sergey_balotnikov/homework1/MainActivity.java
+++ Homework1/app/src/main/java/ru/mail/sergey_balotnikov/homework1/MainActivity.java
@@ -2,16 +2,13 @@ package ru.mail.sergey_balotnikov.homework1;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.textclassifier.TextClassifierEvent;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    private int orientation;
-    private int screenSize=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,36 +19,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onResume()
-    {
-        super.onResume();
-        setBackgroundImage(orientation);
-    }
-
-    private void setBackgroundImage(final int orientation)
-    {
-        LinearLayout layout;
-        layout = findViewById(R.id.l_flag);
-
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            layout.setBackgroundResource(R.drawable.flag_landscape);
-        } else if (orientation == Configuration.ORIENTATION_PORTRAIT){
-            layout.setBackgroundResource(R.drawable.flag_portrait);
-        } else if (screenSize!=0){
-            layout.setBackgroundResource(R.color.colorWhite);
-        }
-    }
-    @Override
-    public void onConfigurationChanged(Configuration configuration){
+    public void onConfigurationChanged(Configuration configuration) {
         super.onConfigurationChanged(configuration);
-        orientation = configuration.orientation;
-        setBackgroundImage(configuration.orientation);
-        if(configuration.screenHeightDp==480){
-            screenSize = configuration.screenHeightDp;
-        } else if(configuration.screenWidthDp==480){
-            screenSize = configuration.screenWidthDp;
-        }
-
+        this.setContentView(R.layout.activity_main);
     }
 
 }
